c8a24d370b0f6eecc693585a2c333a4c7b00b9d5
Mauricio <mauriciolopez097097@gmail.com>
2024-10-26 01:13:28 -0300

    Primer Commit

---
+++ EstacionamientoAustralApi/Migrations/20241026040031_InitialMigration.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EstacionamientoAustralApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cocheras",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false),
+                    Deshabilitada = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Eliminada = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cocheras", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Estacionamientos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Patente = table.Column<string>(type: "TEXT", nullable: false),
+                    HoraIngreso = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    HoraEgreso = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Costo = table.Column<decimal>(type: "TEXT", nullable: false),
+                    IdUsuarioIngreso = table.Column<int>(type: "INTEGER", nullable: false),
+                    IdUsuarioEgreso = table.Column<int>(type: "INTEGER", nullable: true),
+                    IdCochera = table.Column<int>(type: "INTEGER", nullable: false),
+                    Eliminado = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estacionamientos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tarifas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false),
+                    Valor = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tarifas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: false),
+                    Apellido = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    Eliminado = table.Column<bool>(type: "INTEGER", nullable: false),
+                    EsAdmin = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cocheras");
+
+            migrationBuilder.DropTable(
+                name: "Estacionamientos");
+
+            migrationBuilder.DropTable(
+                name: "Tarifas");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- EstacionamientoAustralApi/Program.cs
+++ EstacionamientoAustralApi/Program.cs
@@ -1,15 +1,78 @@
+using Data.context;
+using Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Services;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Inyección de dependencias para Repositorios
+builder.Services.AddScoped<IUserRepository>();
+builder.Services.AddScoped<ITarifaRepository>();
+builder.Services.AddScoped<IEstacionamientoRepository>();
+builder.Services.AddScoped<ICocheraRepository>();
+// Inyección de dependencias para Servicios
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<TarifaService>();
+builder.Services.AddScoped<EstacionamientoService>();
+builder.Services.AddScoped<CocheraService>();
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Configuración de Swagger para incluir JWT
+builder.Services.AddSwaggerGen(setupAction =>
+{
+    setupAction.AddSecurityDefinition("EstacionamientoAustralApi", new OpenApiSecurityScheme() //Esto va a permitir usar swagger con el token.
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        Description = "Acá pegar el token generado al loguearse."
+    });
+
+    setupAction.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "EstacionamientoAustralApi" } //Tiene que coincidir con el id seteado arriba en la definición
+                }
+            , new List<string>() }
+    });
+});
+// Configuración de autenticación con JWT
+builder.Services.AddAuthentication("Bearer") //"Bearer" es el tipo de auntenticación que tenemos que elegir después en PostMan para pasarle el token
+    .AddJwtBearer(options => //Acá definimos la configuración de la autenticación. le decimos qué cosas queremos comprobar. La fecha de expiración se valida por defecto.
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Authentication:Issuer"],
+            ValidAudience = builder.Configuration["Authentication:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["Authentication:SecretForKey"]))
+        };
+    }
+);
+
+// Configuración de Entity Framework y base de datos
+builder.Services.AddDbContext<ApplicationContext>(options =>
+    options.UseSqlite(
+        builder.Configuration["ConnectionStrings:DBConnectionString"],
+        b => b.MigrationsAssembly("EstacionamientoAustralApi")));
+
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuración del pipeline de la aplicación
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
